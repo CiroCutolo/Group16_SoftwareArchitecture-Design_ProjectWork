e8ad3b3f530cabb0da42a4fe1a3306732eb0d264
Box92 <150912140+Box92@users.noreply.github.com>
2025-05-17 20:31:45 +0200

    Gestione click per forme e colore
    Gestione del click per l'inserimento forme.
    Gestione del colore del perimetro delle figure.
    Aggiunti i pulsanti per l'aggiunta delle forme.
    Piccoli fix per il colore di riempimento.
    Piccolo fix per gitignore cosi che non modifichi i file di configurazione personali

--- DrawingApplication/src/Shapes/ShapeFactory.java
+++ DrawingApplication/src/Shapes/ShapeFactory.java
@@ -1,5 +1,7 @@
 package Shapes;
 
+import javafx.scene.paint.Color;
+
 /**
  * La classe ShapeFactory si occupa della creazione delle varie forme concrete (LineShape,
  * RectangleShape, EllipseShape) a partire da una stringa identificativa. 
@@ -14,7 +16,7 @@ package Shapes;
 public class ShapeFactory {
     
     /**
-     * Metodo per la creazione di una forma concreta.
+     * Metodo per la creazione di una forma concreta senza specificarne i colori di bordo e riempimento
      * 
      * @param selected_shape Il tipo di forma da creare (LINE, RECTANGLE, ELLIPSE)
      * @param x1 Coordinata X iniziale
@@ -25,16 +27,43 @@ public class ShapeFactory {
      * @throws IllegalArgumentException se la forma specificata non è supportata
      */
     public static Shape createShape(String selected_shape, double x1, double y1, double x2, double y2) {
+        return createShape(selected_shape, x1, y1, x2, y2, Color.BLACK, Color.TRANSPARENT);
+    }
+    
+    /**
+     * Metodo per la creazione di una forma concreta specificandone i colori di bordo e riempimento
+     * 
+     * @param selected_shape Il tipo di forma da creare (LINE, RECTANGLE, ELLIPSE)
+     * @param x1 Coordinata X iniziale
+     * @param y1 Coordinata Y iniziale
+     * @param x2 Coordinata X finale
+     * @param y2 Coordinata Y finale
+     * @param strokeColor Colore del bordo della figura
+     * @param fillColor Colore di riempimento della figura
+     * @return Un oggetto di tipo Shape
+     * @throws IllegalArgumentException se la forma specificata non è supportata
+     */
+    public static Shape createShape(String selected_shape, double x1, double y1, double x2, double y2,Color strokeColor, Color fillColor) {
+        Shape shape;
+        
         switch (selected_shape) {
             case "LINE":
-                return new LineShape(x1, y1, x2, y2);
+                shape = new LineShape(x1, y1, x2, y2);
+                break;
             case "RECTANGLE":
-                return new RectangleShape(x1, y1, x2, y2);
+                shape = new RectangleShape(x1, y1, x2, y2);
+                break;
             case "ELLIPSE":
-                return new EllipseShape(x1, y1, x2, y2);
+                shape = new EllipseShape(x1, y1, x2, y2);
+                break;
             default:
                 throw new IllegalArgumentException("Forma inserita, non supportata: " + selected_shape);
         }
+        
+        // Applica i colori se non nulli
+        if (strokeColor != null) shape.setPerimetralColor(strokeColor);
+        if (fillColor != null) shape.setInternalColor(fillColor);
+        return shape;
     }
     
 }
--- DrawingApplication/src/drawingapplication/FXMLDocumentController.java
+++ DrawingApplication/src/drawingapplication/FXMLDocumentController.java
@@ -4,6 +4,8 @@
  */
 package drawingapplication;
 
+import Shapes.Shape;
+import Shapes.ShapeFactory;
 import java.awt.event.MouseEvent;
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -12,14 +14,21 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.Labeled;
 import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.control.Tooltip;
 import javafx.scene.effect.DropShadow;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.Region;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
 import javafx.util.Duration;
 
 
@@ -46,7 +55,13 @@ public class FXMLDocumentController implements Initializable {
     private Circle purpleButton, blackButton, pinkButton, yellowButton, greenButton, blueButton, redButton, whiteButton, cyanButton;
     @FXML
     private HBox colorToolbarSection;
-    
+    @FXML
+    private ToggleGroup shapeToggleGroup;
+    @FXML
+    private ToggleButton lineButton,rectangleButton,ellipseButton;
+    private double startX, startY;
+    private String selectedShapeType = null; 
+    private javafx.scene.shape.Shape previewShape = null;
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
@@ -70,8 +85,85 @@ public class FXMLDocumentController implements Initializable {
         redButton.setOnMouseClicked(event -> handleColorSelection(Color.RED));
         whiteButton.setOnMouseClicked(event -> handleColorSelection(Color.WHITE));
         cyanButton.setOnMouseClicked(event -> handleColorSelection(Color.CYAN));
+        
+        
+        //Evita che il disegno vada oltre l'area di disegno
+        drawingPane.setClip(new Rectangle(drawingPane.getPrefWidth(), drawingPane.getPrefHeight()));
+        
+        //Imposta tooltip e immagine dei pulsanti delle forme
+        setTooltipAndImage(rectangleButton,"Rectangle","rectangle_icon.png");
+        setTooltipAndImage(ellipseButton,"Ellipse","ellipse_icon.png");
+        setTooltipAndImage(lineButton,"Line","line_icon.png");
+        
+        //Quando premo nell'are di disegno prende la posizione iniziale del mouse
+        drawingPane.setOnMousePressed(event -> {
+        if (selectedShapeType == null) return;
+        startX = event.getX();
+        startY = event.getY();
+    });
+
+    //Trascinamento mouse
+    drawingPane.setOnMouseDragged(event -> {
+        if (selectedShapeType == null) return;
+        
+        //Evita che la preview possa andare oltre l'area di disegno
+        double endX = Math.max(0, Math.min(event.getX(), drawingPane.getWidth()));
+        double endY = Math.max(0, Math.min(event.getY(), drawingPane.getHeight()));
+
+        //Crea la forma provvisoria (preview)
+        Shape temp = ShapeFactory.createShape(selectedShapeType, startX, startY, endX, endY);
+        javafx.scene.shape.Shape fxTempShape = temp.toFXShape();
+
+        //Stile del preview
+        fxTempShape.getStrokeDashArray().addAll(5.0, 5.0); // tratteggiata
+        fxTempShape.setStroke(Color.GRAY);
+        fxTempShape.setFill(Color.TRANSPARENT);
+
+        //Rimuove la forma precedente di preview se presente
+        if (previewShape != null) {
+            drawingPane.getChildren().remove(previewShape);
+        }
+
+        previewShape = fxTempShape;
+        drawingPane.getChildren().add(previewShape);
+    });
+    
+    // Rilascio del mouse
+    drawingPane.setOnMouseReleased(event -> {
+        if (selectedShapeType == null) return;
+        
+        //Non permette alla figura di uscire dall'area di disegno
+        double endX = Math.max(0, Math.min(event.getX(), drawingPane.getWidth()));
+        double endY = Math.max(0, Math.min(event.getY(), drawingPane.getHeight()));
+
+        //Rimuove la preview
+        if (previewShape != null) {
+            drawingPane.getChildren().remove(previewShape);
+            previewShape = null;
+        }
+
+        //Crea la forma definitiva
+        Shape finalShape = ShapeFactory.createShape(selectedShapeType, startX, startY, endX, endY,perimetralColor,fillingColor);
+        javafx.scene.shape.Shape fxFinalShape = finalShape.toFXShape();
+
+        drawingPane.getChildren().add(fxFinalShape);
+    });
+        
     }  
 
+    //Todo - L'UI permette di selezionare contemporaneamnte perimeter e fill,
+    //       quindi vanno in conflitto i due. Le diverse opzioni sono:
+    //       1) A livello UI inserire mutua esclusione tra fill e perimeter
+    //       2) Togliere l'else if e gestirla in maniera diversa (magari se ho
+    //          selezionato sia fill che perimeter allora cambio il colore di entrambi)
+    
+    //Todo - Magari implementare modi per resettare il colore a quello predefinito
+    //       (cioè fill trasparente o bianco e perimetro nero). Perchè ora se disattivo
+    //       i pulsanti di fill e border i colori rimangono gli ultimi selezionati.
+    //       Opzioni:
+    //       1) Inserire un pulsante che resetti ai colori predefiniti
+    //       2) Quando il pulsante "fill" non è selezionato allora usa il colore predefinito
+    //          (lo stesso per il "border" o "perimeter").
     private void handleColorSelection(Color color) {
         
         if (perimeterRadio.isSelected()) {
@@ -102,6 +194,7 @@ public class FXMLDocumentController implements Initializable {
     }
 
     @FXML
+    //Todo - L'effetto non si nota abbastanza. E' difficile capire quale colore è attivo (ultimo selezionato).
     private void clickEffect(javafx.scene.input.MouseEvent event) {
         Circle circle = (Circle) event.getSource();
         ScaleTransition scaling = new ScaleTransition(Duration.millis(100), circle);
@@ -110,5 +203,49 @@ public class FXMLDocumentController implements Initializable {
         scaling.setAutoReverse(true);
         scaling.setCycleCount(2);
         scaling.play();
+        
+    }
+
+    
+    @FXML
+    private void selectRectangle(ActionEvent event) {
+        if(rectangleButton.isSelected()){
+            selectedShapeType = "RECTANGLE";
+        } else{
+            selectedShapeType = null;
+        }
+    }
+
+    @FXML
+    private void selectEllipse(ActionEvent event) {
+        if(ellipseButton.isSelected()){
+            selectedShapeType = "ELLIPSE";
+        } else{
+            selectedShapeType = null;
+        }
+    }
+
+    @FXML
+    private void selectLine(ActionEvent event) {
+        if(lineButton.isSelected()){
+            selectedShapeType = "LINE";
+        } else{
+            selectedShapeType = null;
+        }
+    }
+    
+        private void setTooltipAndImage(ToggleButton button, String tooltip, String imageFile){
+        button.setTooltip(new Tooltip(tooltip));
+        setImage(button,imageFile,22);
+    }
+    
+    private void setImage(Labeled node,String imageFile,int dimension){
+        Image imageOk = new Image(getClass().getResourceAsStream("/icons/" + imageFile));
+        //Image imageOk = new Image("file:src/icons/" + imageFile);
+        ImageView img = new ImageView(imageOk);
+        img.setPreserveRatio(true);
+        img.setFitHeight(dimension);
+        img.setFitWidth(dimension);
+        node.setGraphic(img);
     }
 }
